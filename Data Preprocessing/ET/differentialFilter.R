9166cf49b8112b41cc45195a48c75a0bcb477d1e
Kok Herng <herngkok@hotmail.com>
2022-03-11 12:10:03 +0800

    Create differentialFilter.R
    Script to filter unrelated records from each trait, add new up/down regulated column, and merge into single file

---
+++ Data Preprocessing/ET/differentialFilter.R
@@ -0,0 +1,78 @@
+#script to filter unrelated records from each trait, 
+#add new up/down regulated column, and
+#merge into single file
+
+library(readr)
+library(tidyverse)
+library(openxlsx)
+
+coldDifferential <- read_delim("ebiColdDifferential.tsv", "\t", 
+                               escape_double = FALSE, 
+                               col_types = cols(`t-statistic` = col_double()), 
+                               trim_ws = TRUE)
+droughtDifferential <- read_delim("ebiDroughtDifferential.tsv", "\t", 
+                                  escape_double = FALSE, 
+                                  col_types = cols(`t-statistic` = col_double()), 
+                                  trim_ws = TRUE)
+heatDifferential <- read_delim("ebiHeatDifferential.tsv", "\t", 
+                               escape_double = FALSE, 
+                               col_types = cols(`t-statistic` = col_double()), 
+                               trim_ws = TRUE)
+saltDifferential <- read_delim("ebiSaltDifferential.tsv", "\t", 
+                               escape_double = FALSE, 
+                               col_types = cols(`t-statistic` = col_double()), 
+                               trim_ws = TRUE)
+stressDifferential <- read_delim("ebiStressDifferential.tsv", "\t", 
+                                 escape_double = FALSE, 
+                                 col_types = cols(`t-statistic` = col_double()), 
+                                 trim_ws = TRUE)
+submergencyDifferential <- read_delim("ebiSubmergencyDifferential.tsv", "\t", 
+                                      escape_double = FALSE, col_types = cols(`t-statistic` = col_double()), 
+                                      trim_ws = TRUE)
+
+coldFiltered <- coldDifferential %>% 
+  filter(str_detect(Comparison, 'cold | chill | celsius')) %>% #filter only related conditions
+  mutate('Up/Down regulated' = case_when(
+    `log_2 fold change` > 0 ~ 'Up regulated',
+    `log_2 fold change` < 0 ~ 'Down regulated'
+  ))
+
+droughtFiltered <- droughtDifferential %>% 
+  filter(str_detect(Comparison, 'drought | dry | PEG')) %>% #filter only related conditions
+  mutate('Up/Down regulated' = case_when(
+    `log_2 fold change` > 0 ~ 'Up regulated',
+    `log_2 fold change` < 0 ~ 'Down regulated'
+  ))
+
+heatFiltered <- heatDifferential %>% #no filtering, use all records
+  mutate('Up/Down regulated' = case_when(
+    `log_2 fold change` > 0 ~ 'Up regulated',
+    `log_2 fold change` < 0 ~ 'Down regulated'
+  ))
+
+saltFiltered <- saltDifferential %>% 
+  filter(str_detect(Comparison, 'cadmium | salt | alkali | sodium')) %>% #filter only related conditions
+  mutate('Up/Down regulated' = case_when(
+    `log_2 fold change` > 0 ~ 'Up regulated',
+    `log_2 fold change` < 0 ~ 'Down regulated'
+  ))
+
+stressFiltered <- stressDifferential %>% #no filtering, use all records
+  mutate('Up/Down regulated' = case_when(
+    `log_2 fold change` > 0 ~ 'Up regulated',
+    `log_2 fold change` < 0 ~ 'Down regulated'
+  ))
+
+submergencyFiltered <- submergencyDifferential %>% 
+  filter(str_detect(Comparison, 'anaerobic | submergence | oxygen')) %>% #filter only related conditions
+  mutate('Up/Down regulated' = case_when(
+    `log_2 fold change` > 0 ~ 'Up regulated',
+    `log_2 fold change` < 0 ~ 'Down regulated'
+  ))
+
+sheetNames <- list('Cold' = coldFiltered, 'Drought' = droughtFiltered, 'Heat' = heatFiltered, 'Salt' = saltFiltered,
+                   'Stress' = stressFiltered, 'Submergency' = submergencyFiltered)
+write.xlsx(sheetNames, file = 'differentialFiltered.xlsx')
+
+#extra steps done in Excel to further exclude out unrelated rows like
+#other stresses and disease traits
