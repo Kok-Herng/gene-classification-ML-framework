ffaea7e026553c12f606409bdfc20af93584b446
Kok Herng <herngkok@hotmail.com>
2022-04-10 14:57:35 +0800

    Add files via upload

---
+++ Training Data/trainingData.R
@@ -0,0 +1,71 @@
+library(tidyverse)
+library(openxlsx)
+
+rawFeatures <- read_excel("C:/Users/Bioinfo/Desktop/kok/OneDrive - Universiti Malaya/Final Year Project/Personal Laptop/Masterlists/rawFeatures.xlsx")
+
+coexpAndPpi <- function(filename){ #check if gene present in coExp and PPI
+  a <- filename %>% 
+    select(a_OsID) %>%
+    rename(OsID = a_OsID)
+  
+  b <- filename %>% 
+    select(b_OsID) %>%
+    rename(OsID = b_OsID)
+  
+  temp <- rbind(a,b) %>% #select and merge gene a and b together
+    distinct() #remove duplicates
+  
+  return(temp)
+}
+
+#Co-expression Network----
+coExpression <- read_excel("C:/Users/Bioinfo/Desktop/kok/OneDrive - Universiti Malaya/Final Year Project/Lab Computer/Data Preprocessing/coExpression.xlsx")
+tempCoExp <- coexpAndPpi(coExpression)
+rawFeatures$CoExpression <- as.integer(rawFeatures$OsID %in% tempCoExp$OsID) #check if OsID present in coExpression
+
+#PPI Network----
+ppiNetwork <- read_excel("C:/Users/Bioinfo/Desktop/kok/OneDrive - Universiti Malaya/Final Year Project/Lab Computer/Data Preprocessing/ppiNetwork.xlsx")
+tempPPI <- coexpAndPpi(ppiNetwork)
+rawFeatures$PPI <- as.integer(rawFeatures$OsID %in% tempPPI$OsID) #check if OsID present in PPI
+
+#ET----
+groupByTraits <- read_excel("C:/Users/Bioinfo/Desktop/kok/OneDrive - Universiti Malaya/Final Year Project/Lab Computer/Data Preprocessing/ET/groupByTraits.xlsx", sheet = "All")
+
+groupByTraits$`Up/Down regulated` <- make.names(groupByTraits$`Up/Down regulated`) #modify values to valid names
+
+groupByTraits <- groupByTraits %>%
+  mutate(ET = case_when(
+    `Up/Down regulated`=="Down.regulated" ~ 1,
+    `Up/Down regulated`=="Up.regulated" ~ 2))
+
+rawFeatures <- groupByTraits %>%
+  select(OsID,`log_2 fold change`,ET) %>%
+  right_join(rawFeatures) %>% #keep all genes from rawFeatures
+  mutate_at(vars(`log_2 fold change`,ET), ~replace_na(., 0)) %>% #replace NAs from log_2 fold change and ET to 0 (no expression)
+  arrange(OsID) %>% #sort OsID in ascending order
+  mutate(Class = as.integer(fct_inorder(OsID))) %>% #labeling according to OsID
+#alternative = labeling according to traits
+  distinct(.keep_all = T) #keep only unique values
+
+#EV----
+evPatternTest <- evPattern %>%
+  split(evPattern$OsID) %>% #split into list of data frames according to OsID
+  map(function(x) summarise(x,across(where(is.character), ~OsID),across(where(is.numeric), sum,na.rm = TRUE))) %>% #add new row for sum value of each column
+  bind_rows() %>% #combine list of data frames to a single data frame
+  distinct() %>%
+  mutate_if(is.numeric, ~replace(., . == 0, NA))#replace zero with NA
+
+OsID <- evPatternTest[,1, drop=FALSE] #extract OsID column
+
+evPattern <- as.data.frame(t(apply(evPatternTest[,2:16], 1, function(x) replace(x, x != max(x, na.rm = TRUE), 0)))) %>% #keep only max value of row and convert other values to 0, ignoring OsID column
+  mutate_if(is.numeric, ~1 * (. != 0)) %>% #convert all non-zero numeric values (NA not affected) to 1
+  add_column(OsID, .before = 1) #add OsID column to front
+
+rawFeatures <- rawFeatures %>%
+  left_join(evPattern) %>%
+  distinct(.keep_all = T)
+
+#output results to file----
+write.xlsx(rawFeatures,"trainingData.xlsx")
+
+#further formatting is done in Excel, ie renaming columns
