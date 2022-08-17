31a212b9c3b32e7a2d4fdfbf1639ce0a7cb730e7
Kok-Herng <herngkok@hotmail.com>
2022-08-17 10:42:15 +0800

    Add code to replace "LOC.Gene.ID" as "OsID"

--- Data Curation/EV/evPattern.R
+++ Data Curation/EV/evPattern.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(openxlsx)
 
 selectedProj <- read.xlsx("Data Extraction/EV/fltr_sra_fpkm.xlsx", sheet = "fltr_sra_fpkm")
+rapdb_msu <- read_delim("rapdb_msu.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
 
 evPatternTemp <- selectedProj %>%
   select(LOC.Gene.ID, Development.stage, Tissue, Expression.value) %>%
@@ -26,6 +27,11 @@ evPattern <- evPatternTemp %>%
   lapply(bind_rows) %>% #convert individual list to dataframe, making a list of dataframes
   bind_rows() %>% #convert the list of dataframes to a single dataframe
   filter_at(-1, any_vars(!is.na(.))) %>% #remove rows with all NA except for LOC.Gene.ID column
-  write.xlsx("evPattern.xlsx") #write output to file
+  # Replace "LOC.Gene.ID" as "OsID" 
+  left_join(rapdb_msu, by = c("LOC.Gene.ID"="LocID")) %>% 
+  select(-LOC.Gene.ID) %>%
+  select(OsID, everything()) #move "OsID" column to the front 
+
+write.xlsx(evPattern, "evPattern.xlsx") #write output to file
 
 #further formatting is done in Excel, ie rename column names and merge tissue
