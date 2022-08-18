f4a99169ed8b518f6346c7284aa916d26a9f21ad
Kok-Herng <herngkok@hotmail.com>
2022-08-18 21:37:02 +0800

    Update comment

--- Data Curation/ET/differentialFilter.R
+++ Data Curation/ET/differentialFilter.R
@@ -72,7 +72,7 @@ submergencyFiltered <- submergencyDifferential %>%
 
 sheetNames <- list('Cold' = coldFiltered, 'Drought' = droughtFiltered, 'Heat' = heatFiltered, 'Salt' = saltFiltered,
                    'Stress' = stressFiltered, 'Submergency' = submergencyFiltered)
-write.xlsx(sheetNames, file = 'differentialFiltered.xlsx') #write ouput to file
+write.xlsx(sheetNames, file = 'differentialFiltered.xlsx') #write output to file
 
 #extra steps done in Excel to further exclude out unrelated rows like
 #other stresses and disease traits
