62aaf8c6abb5628a3d7b937dc2e888102bd8f4a1
Kok-Herng <herngkok@hotmail.com>
2022-08-24 21:46:21 +0800

    Update comments

--- Data Extraction/EV/scriptEV.py
+++ Data Extraction/EV/scriptEV.py
@@ -7,40 +7,41 @@ import os
 for file in os.listdir(os.getcwd()): #for every file in current working directory
 	if file.endswith(".csv"):
 		
-		fIn = open(file, "r+") #open each csv files in directory
-		fOut = open("outEV2.csv","a",newline='') #output each lines to one single output file using append
+		fIn = open(file, "r+") #open csv files
+		fOut = open("outEV2.csv","a",newline='') #open file for output
 		#newline='' to disable extra newlines written to output file
 
 		data = [] #empty list to store ALL lines
 		cnt = 0
 
-		lines = fIn.readlines() #read all lines
+		lines = fIn.readlines() #read all lines into a list of strings
 		last = lines[-1] #last line in the file
 		
 		for line in lines:
-			line.strip() #Remove spaces at the beginning and end of string
+			line.strip() #remove spaces at the beginning and end of string
 			print(line)
 			
-			head = re.match(r'""(.*)', line, re.IGNORECASE) #return true for string starts with "" and followed
-			#by multiple any characters (2nd line onwards in file)
+			head = re.match(r'""(.*)', line, re.IGNORECASE) #match string starts with "" and followed by zero or more any characters (2nd line onwards in file)
 			
 			if head: #true
-				tempRow = [] #empty list to store ONE line at a time, revert back to empty after each line
+				tempRow = [] #empty list to store ONE line at a time, revert to empty after each line
 				headerLine = head.group(1) #return the SECOND subgroup for the re match (in this case, LOCxxxxx and so on)
 				print (headerLine)
 
 				terms = headerLine.split(',') #split string into list with comma as seperator
 				print(terms)	
 
-				geneId = terms[1]; geneId = geneId[1:-1] #extract the SECOND item in list and remove " at the beginning and end
+				#extract the respective gene ID, tissue, development stage and remove " at the beginning and end
+				geneId = terms[1]; geneId = geneId[1:-1] 
 				tissue = terms[-2]; tissue = tissue[1:-1]
 				developmentStage = terms[-3]; developmentStage = developmentStage[1:-1]
 				
+				#weird but it works ¯\_(ツ)_/¯
 				if line is last: #check if current line is last in file
-					ev = terms[-1]; ev = ev[1:-1] #ensure no extra digit is removed since no /r for last line
+					ev = terms[-1]; ev = ev[1:-1] #ensure no extra digit is removed since no newline for last line
 				
 				else:
-					ev = terms[-1]; ev = ev[1:-1] #remove /r for rest of the lines
+					ev = terms[-1]; ev = ev[1:-1] #remove newline for rest of the lines
 
 				tempRow = [geneId, developmentStage, tissue, ev] #put extracted items into a list
 				print(tempRow)
